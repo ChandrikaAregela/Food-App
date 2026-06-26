1c579c78f287a56d6bb07e5403293e4f07dfa0de
Aregela Naga Chandrika <nagachandrikaaregela@gmail.com>
2026-06-26 20:45:12 +0530

    Add remove from cart functionality

--- cart.py
+++ cart.py
@@ -1,19 +1,30 @@
 class Cart:
+
     def __init__(self):
         self.items = []
 
     def add_item(self, food):
         self.items.append(food)
 
+    def remove_item(self, food_id):
+        for item in self.items:
+            if item["food_id"] == food_id:
+                self.items.remove(item)
+                print(f"{item['name']} removed from the cart.")
+                return
+        print("Food not found in the cart.")
+
     def view_cart(self):
-        total = 0
         if len(self.items) == 0:
-            print("Cart is Empty")
+            print("Cart is Empty!")
             return
 
-        print("\nYOUR CART")
-
+        total = 0
+        print("\n===== YOUR CART =====")
         for item in self.items:
-            print(f"{item['name']} - ₹{item['price']}")
+            print(f"ID: {item['food_id']}")
+            print(f"Food : {item['name']}")
+            print(f"Price: ₹{item['price']}")
+            print("----------------------")
             total += item["price"]
-        print(f"\nTotal = ₹{total}")
+        print(f"Total = ₹{total}")
--- main.py
+++ main.py
@@ -2,88 +2,132 @@ import json
 from cart import Cart
 from order import place_order
 from food import view_foods
+
 cart = Cart()
 
 
 def register():
     username = input("Enter Username: ")
     password = input("Enter Password: ")
-    
-    user = { "username": username, "password": password}
-    with open("users.json","r") as file:
+
+    user = {
+        "username": username,
+        "password": password
+    }
+
+    with open("users.json", "r") as file:
         users = json.load(file)
+
     users.append(user)
-    
-    with open("users.json","w") as file:
-        json.dump(users,file,indent=4)
-    print("Registration is Successful")
+
+    with open("users.json", "w") as file:
+        json.dump(users, file, indent=4)
+
+    print("Registration Successful")
+
 
 def login():
     username = input("Enter Username: ")
     password = input("Enter Password: ")
-    
-    with open("users.json","r") as file:
+
+    with open("users.json", "r") as file:
         users = json.load(file)
-    
+
     for user in users:
         if user["username"] == username and user["password"] == password:
             print("Login Successful")
             return username
-    print("Invalid Username or Password") 
-    return None  
+
+    print("Invalid Username or Password")
+    return None
+
 
 def add_to_cart():
     food_id = int(input("Enter Food ID: "))
-    
-    with open("foods.json","r") as file:
+
+    with open("foods.json", "r") as file:
         foods = json.load(file)
-        
+
     for food in foods:
         if food["food_id"] == food_id:
             cart.add_item(food)
-            print(" added successfully ")
+            print("Food added to cart successfully.")
             return
-    print("Food not found")  
+
+    print("Food not found.")
 
 
+def remove_from_cart():
+    if len(cart.items) == 0:
+        print("Cart is Empty!")
+        return
 
+    cart.view_cart()
+
+    food_id = int(input("Enter Food ID to remove: "))
+    cart.remove_item(food_id)
 
 
 while True:
-    print("--- FOOD APP ---")  
-    print("1.Register")
-    print("2.Login")
-    print("3.Exit")
-    choice = input("Enter Choice : ")
-    
+
+    print("\n===== FOOD APP =====")
+    print("1. Register")
+    print("2. Login")
+    print("3. Exit")
+
+    choice = input("Enter Choice: ")
+
     if choice == "1":
         register()
-    elif choice == "2":
-       username = login()
-       if username:
-           while True:
-               print("\n ====USER MENU===")
-               print("1. View Foods")
-               print("2. Add to Cart")
-               print("3. View cart")
-               print("4. Place Order")
-               print("5. Logout")
-               option = input("Enter Choice: ")
-               if option == "1":
-                   view_foods()
-               elif option =="2":
-                   add_to_cart()
-               elif option == "3":
-                   cart.view_cart()
-               elif option == "4":
-                    place_order(username, cart.items)
-                    cart.items.clear()
 
-               elif option == "5":
+    elif choice == "2":
+
+        username = login()
+
+        if username:
+
+            while True:
+
+                print("\n===== USER MENU =====")
+                print("1. View Foods")
+                print("2. Add to Cart")
+                print("3. View Cart")
+                print("4. Remove from Cart")
+                print("5. Place Order")
+                print("6. Logout")
+
+                option = input("Enter Choice: ")
+
+                if option == "1":
+                    view_foods()
+                    
+                elif option == "2":
+                    add_to_cart()
+
+                elif option == "3":
+                    cart.view_cart()
+
+                elif option == "4":
+                    remove_from_cart()
+
+                elif option == "5":
+                    if len(cart.items) == 0:
+                        print("Cart is Empty!")
+
+                    else:
+                        place_order(username, cart.items)
+                        cart.items.clear()
+
+                elif option == "6":
+                    print("Logged Out Successfully!")
                     break
 
+                else:
+                    print("Invalid Choice")
+
     elif choice == "3":
-        print("Thank You")
+        print("Thank You!")
         break
-                
-    
+
+    else:
+        print("Invalid Choice")
